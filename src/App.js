7952c47488373a547c8ecd5c5755aeb11cb069e8
Irfanahmedkhan <64011793+Irfanahmedkhan@users.noreply.github.com>
2020-07-24 11:52:24 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import Page1 from './Page1'
+import Page2 from './Page2'
+// import Page1 from './Page1'
 
 function App() {
   return (
     <div className="App">
 
-<Page1 />
+      <Page2 />
+      {/* <Page1 /> */}
 
     </div>
   );
--- src/Page1.js
+++ src/Page1.js
@@ -70,26 +70,23 @@ function Page1() {
 
         <div className="recurrence">
           <h4>Insert Recurrence</h4>
-
           <label>Start Date</label>
           <input type="date" className="startdate" />
-
           <label>Stop Date</label>
           <input type="date" className="stopdate" />
-
           <label>Recurrence</label>
           <select className="daily" className="daily">
             <option value="01">Daily</option>
             <option value="02">2 time</option>
           </select>
-
           <label>Every</label>
           <span>
-                      <input type="every" className="every" placeholder="1" />&nbsp;Day(s)
+            <input type="every" className="every" placeholder="1" />
+            &nbsp;Day(s)
           </span>
-
           <label>Value</label>
-<div className='value'></div>          <div className="button">
+          <div className="value"></div>{" "}
+          <div className="button">
             <button>Insert/Update Recurrence</button>
             <button>Delete by Recurrence</button>
           </div>
